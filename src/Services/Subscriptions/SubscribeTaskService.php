290b5f8b12c6569d59b5f7cd8b5e6879c328c05f
Florian Würtenberger <florian@wdigital.ch>
2024-10-07 11:37:31 +0200

    - SubscribeTaskService updated.

--- src/Services/Subscriptions/SubscribeTaskService.php
+++ src/Services/Subscriptions/SubscribeTaskService.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) - WDigital - 2022.
+ * Copyright (c) - WDigital - 2024.
  * @link https://wdigital.ch
  * @developer Florian Würtenberger <florian@wdigital.ch>
  */
@@ -62,51 +62,15 @@ class SubscribeTaskService extends KlickTippBaseService
 			return $this->updateSubscribe($email, $this->searchSubscriberByEmail($email)['contactCloudId'], $fields, $optionalFields);
 		}
 
-		// Füge Daten zu Felder aus der KlickTipp ContactCloud.
-		$fieldTaskServiceInstance     = FieldTaskService::getInstance();
-		$getFieldListFromContactCloud = $fieldTaskServiceInstance->fieldList();
-
-		$rebuildFieldFromContactCloud = KtFieldHelper::rebuildFieldsFromContactCloud($getFieldListFromContactCloud);
 		$requestFieldArray            = [];
-
-		// Entfernt die Underline im Key und schreibt den ersten Buchstaben nach dem Underline gross.
-		foreach ($fields as $fieldKey => $fieldValue) {
-			//dd(strpos($fieldKey, '_') !== false);
-			if (str_contains($fieldKey, '_')) {
-				$explodeUnderlineFromInputFields = explode('_', $fieldKey);
-				$keyBeforeUnderline              = $explodeUnderlineFromInputFields[0];
-				$keyAfterUnderline               = $explodeUnderlineFromInputFields[1];
-				$keyAfterRecomposed              = $keyBeforeUnderline . ucfirst($keyAfterUnderline);
-			} else {
-				$keyAfterRecomposed = $fieldKey;
-			}
-
-			foreach ($rebuildFieldFromContactCloud as $fieldFromContactCloudKey => $fieldFromContactCloudValue) {
-				$inputFieldRebuildToContactCloudStringFormat = KtFieldHelper::rebuildField($keyAfterRecomposed);
-
-				// Überprüft ob der ContactCloudKey mit dem gerade übergebenen Feld zusammenpasst und setzt den Wert zum jeweiligen Feld.
-				if ($fieldFromContactCloudValue === ucfirst($keyAfterRecomposed)) {
-					$requestFieldArray['fields'][$inputFieldRebuildToContactCloudStringFormat] = $fieldValue;
-				}
-
-				if (!empty($optionalFields)) {
-					foreach ($getFieldListFromContactCloud as $contactCloudFieldKey => $contactCloudFieldValue) {
-						foreach ($optionalFields as $optionalFieldNameKey => $optionalFieldNameValue) {
-							if ($contactCloudFieldValue === $optionalFieldNameKey) {
-								$requestFieldArray['fields'][$contactCloudFieldKey] = $optionalFieldNameValue;
-							}
-						}
-					}
-				}
-			}
-		}
 
 		$requestFieldArray['email']     = $email;
 		$requestFieldArray['listid']    = $subscriptionProcessId;
 		$requestFieldArray['tagid']     = $tagId;
 		$requestFieldArray['smsnumber'] = ($smsNumber != null) ? $smsNumber : '';
+		$requestFieldArray['fields']    = $fields;
 
-		$ktTagResponse = $this->httpClient->post('subscriber.json', $requestFieldArray);
+		$ktTagResponse = $this->httpClient->post('subscriber', $requestFieldArray);
 
 		if ($ktTagResponse->status() === 200) {
 			return $ktTagResponse->json();
@@ -143,53 +107,13 @@ class SubscribeTaskService extends KlickTippBaseService
 	 * @param string      $newEmail
 	 * @param int         $contactCloudId
 	 * @param array       $fields
-	 * @param array       $optionalFields
 	 * @param string|null $smsNumber
 	 *
 	 * @return array
 	 */
-	public function updateSubscribe(string $newEmail, int $contactCloudId, array $fields = [], array $optionalFields = [], string $smsNumber = null): array
+	public function updateSubscribe(string $newEmail, int $contactCloudId, array $fields = [], string $smsNumber = null): array
 	{
-		// Füge Daten zu Felder aus der KlickTipp ContactCloud.
-		$fieldTaskServiceInstance     = FieldTaskService::getInstance();
-		$getFieldListFromContactCloud = $fieldTaskServiceInstance->fieldList();
-
-		$rebuildFieldFromContactCloud = KtFieldHelper::rebuildFieldsFromContactCloud($getFieldListFromContactCloud);
-		$requestFieldArray            = [];
-
-		// Entfernt die Underline im Key und schreibt den ersten Buchstaben nach dem Underline gross.
-		foreach ($fields as $fieldKey => $fieldValue) {
-
-			if (str_contains($fieldKey, '_')) {
-				$explodeUnderlineFromInputFields = explode('_', $fieldKey);
-				$keyBeforeUnderline              = $explodeUnderlineFromInputFields[0];
-				$keyAfterUnderline               = $explodeUnderlineFromInputFields[1];
-				$keyAfterRecomposed              = $keyBeforeUnderline . ucfirst($keyAfterUnderline);
-			} else {
-				$keyAfterRecomposed = $fieldKey;
-			}
-
-			foreach ($rebuildFieldFromContactCloud as $fieldFromContactCloudKey => $fieldFromContactCloudValue) {
-				$inputFieldRebuildToContactCloudStringFormat = KtFieldHelper::rebuildField($keyAfterRecomposed);
-
-				// Überprüft ob der ContactCloudKey mit dem gerade übergebenen Feld zusammenpasst und setzt den Wert zum jeweiligen Feld.
-				if ($fieldFromContactCloudValue === ucfirst($keyAfterRecomposed)) {
-					$requestFieldArray['fields'][$inputFieldRebuildToContactCloudStringFormat] = $fieldValue;
-				}
-
-				if (!empty($optionalFields)) {
-					foreach ($getFieldListFromContactCloud as $contactCloudFieldKey => $contactCloudFieldValue) {
-						foreach ($optionalFields as $optionalFieldNameKey => $optionalFieldNameValue) {
-							if ($contactCloudFieldValue === $optionalFieldNameKey) {
-								$requestFieldArray['fields'][$contactCloudFieldKey] = $optionalFieldNameValue;
-							}
-						}
-					}
-				}
-			}
-		}
-
-		$ktTagResponse = $this->httpClient->put('/subscriber/' . $contactCloudId, $requestFieldArray);
+		$ktTagResponse = $this->httpClient->put('/subscriber/' . $contactCloudId, $fields);
 
 		if ($ktTagResponse->status() === 200) {
 			return [
